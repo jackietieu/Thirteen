b1b9616b546d299a7e79df80f584931e7edda851
Jackie Tieu <jackietieu@Jackies-MacBook-Pro.local>
2016-09-14 11:29:06 -0700

    played cards render properly

--- human_player.jsx
+++ human_player.jsx
@@ -4,6 +4,7 @@ class HumanPlayer extends React.Component{
   constructor(props){
     super(props);
     this.state = {
+      handCardIds: this.props.hand,
       hand: this.createCards(this.props.hand),
       currentSelection: [],
       currentPlay: []
@@ -15,36 +16,41 @@ class HumanPlayer extends React.Component{
 
     if (!e.currentTarget.classList.contains('selected')) {
       this.setState({
-        currentSelection: this.state.currentSelection.concat(e.currentTarget.className)
+        currentSelection: this.state.currentSelection.concat(e.currentTarget.id)
       });
     } else {
-      let idx = this.state.currentSelection.indexOf(e.currentTarget.className.slice(0, e.currentTarget.className.length - 9));
+      let idx = this.state.currentSelection.indexOf(e.currentTarget.id);
       let tempSelection = this.state.currentSelection;
       tempSelection.splice(idx, 1);
       this.setState({
         currentSelection: tempSelection
       });
     }
+
     e.currentTarget.classList.toggle('selected');
   }
 
-  createCards(hand){
+  createCards(hand, play){
     //CONSIDER MAKING UL/LI LIST INSTEAD
     const suits = ["spades", "clubs", "diamonds", "hearts"];
-
+    // console.log(hand);
     return(
-      hand.map((i, idx) => {
+      hand.sort((a, b) => (
+        a - b
+      )).map((i, idx) => {
         let rank = " rank".concat(i % 13 + 1);
         let suit = suits[i / 13 | 0];
         let offset = {"left":`calc(30px + ${idx * 30}px)`};
+        let key = (play === "play") ? " play" : " hand";
 
         return(
           <div
+            id={i}
             style={offset}
             key={"card ".concat(suit).concat(rank)}
             className={"card ".concat(suit).concat(rank)}
             onClick={this.selectCard.bind(this)}>
-            <div key={"face ".concat(suit).concat(rank)} className="face"></div>
+            <div key={"face ".concat(suit).concat(rank).concat(play)} className="face"></div>
           </div>
         );
       })
@@ -53,8 +59,20 @@ class HumanPlayer extends React.Component{
 
   playCards(e){
     e.preventDefault();
-    this.state.currentSelection.forEach(card => {
+    let oldHandCardIds = this.state.handCardIds;
+    let removeHandCardIds = this.state.currentSelection;
+    let newHandCardIds = [];
 
+    oldHandCardIds.map(id => {
+      if (!removeHandCardIds.includes(id.toString())) {
+        newHandCardIds.push(id);
+      }
+    });
+
+    this.setState({
+      handCardIds: newHandCardIds,
+      hand: this.createCards(newHandCardIds),
+      currentPlay: this.createCards(removeHandCardIds, "play")
     });
   }
 
